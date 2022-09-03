a29a38ee2a908aaa822f1cff9129231bf9ad2358
ayalgimpel <ayalgimpel24@gmail.com>
2022-09-03 15:25:49 +0300

    ayal

--- src/Repository/db.studentsRepository.js
+++ src/Repository/db.studentsRepository.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const util = require("util");
 const writeFile = util.promisify(fs.writeFile);
 const readFile = util.promisify(fs.readFile);
+const { uuid } = require('uuidv4')
 const jsonFileName = "./src/data/StudentsJson.json";
 
 class DBStudentsRepository {
@@ -9,6 +10,15 @@ class DBStudentsRepository {
   async getAllStudents() {
     const data = JSON.parse(await readFile(jsonFileName));
     return data;
+  }
+
+
+  async createStudent(student) {
+    let data = JSON.parse(await readFile(jsonFileName));
+    student.id = uuid();
+    data.Student.push(student);
+    await writeFile(jsonFileName, JSON.stringify(data));
+    return student;
   }
 
   async addStudent(student) {
--- src/routes/studentAnswersRoutes.js
+++ src/routes/studentAnswersRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controller/studentAnswers.js");
+const controller = require("../controller/studentAnswers");
 const asyncHandler = require("../helpers/asyncHandler");
 
 // Get questions from json
